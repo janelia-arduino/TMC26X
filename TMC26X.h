6ad86c9315eb80917b818b3a869769ea2af41eb4
Peter Polidoro <peterpolidoro@gmail.com>
2017-02-17 16:25:43 -0500

    Added drv_conf.

--- TMC26X.h
+++ TMC26X.h
@@ -20,149 +20,24 @@
 class TMC26X
 {
 public:
-  void setup(const size_t cs_pin,
-             const uint8_t clock_frequency);
+  void setup(const size_t cs_pin);
 
-  uint32_t getVersion();
-  bool checkVersion();
-
-  void setStepDirOutput();
-  void setSpiOutput();
-
-  enum Mode
-    {
-      RAMP_MODE=0b00,
-      SOFT_MODE=0b01,
-      VELOCITY_MODE=0b10,
-      HOLD_MODE=0b11,
-    };
-
-  Mode getMode(const size_t motor);
-  void setMode(const size_t motor,
-               const Mode mode);
-
-  uint32_t getVelocityMaxUpperLimitInHz();
-
-  void setLimitsInHz(const size_t motor,
-                     const uint32_t velocity_min,
-                     const uint32_t velocity_max,
-                     const uint32_t acceleration_max);
-
-  uint32_t getAccelerationMaxInHzPerS(const size_t motor);
-  uint32_t getAccelerationMaxUpperLimitInHzPerS(const size_t motor);
-
-  uint32_t getAccelerationActualInHzPerS(const size_t motor);
-
-  uint32_t getVelocityMinInHz(const size_t motor);
-  uint32_t getVelocityMaxInHz(const size_t motor);
-
-  int32_t getVelocityTargetInHz(const size_t motor);
-  void setVelocityTargetInHz(const size_t motor,
-                             const int32_t velocity);
-
-  int32_t getVelocityActualInHz(const size_t motor);
-
-  int32_t getPositionTarget(const size_t motor);
-  void setPositionTarget(const size_t motor,
-                         const int32_t position);
-
-  int32_t getPositionActual(const size_t motor);
-  void setPositionActual(const size_t motor,
-                         const int32_t position);
+  void setStepDirInput();
+  void setSpiInput();
 
   struct Status
   {
-    uint8_t at_target_position_0 : 1;
-    uint8_t switch_left_0 : 1;
-    uint8_t at_target_position_1 : 1;
-    uint8_t switch_left_1 : 1;
-    uint8_t at_target_position_2 : 1;
-    uint8_t switch_left_2 : 1;
-    uint8_t cdgw : 1;
-    uint8_t interrupt : 1;
+    uint8_t stall : 1;
+    uint8_t over_temperature_shutdown : 1;
+    uint8_t over_temperature_warning : 1;
+    uint8_t short_to_ground_a : 1;
+    uint8_t short_to_ground_b : 1;
+    uint8_t open_load_a : 1;
+    uint8_t open_load_b : 1;
+    uint8_t standstill : 1;
   };
 
   Status getStatus();
-
-  struct ReferenceConfiguration
-  {
-    uint8_t disable_stop_l : 1;
-    uint8_t disable_stop_r : 1;
-    uint8_t soft_stop : 1;
-    uint8_t ref_rnl : 1;
-    uint8_t space : 4;
-  };
-
-  ReferenceConfiguration getReferenceConfiguration(const size_t motor);
-
-  void enableLeftSwitchStop(const size_t motor);
-  void disableLeftSwitchStop(const size_t motor);
-
-  void enableRightSwitchStop(const size_t motor);
-  void disableRightSwitchStop(const size_t motor);
-
-  void enableSoftStop(const size_t motor);
-  void disableSoftStop(const size_t motor);
-
-  void setReferenceSwitchToLeft(const size_t motor);
-  void setReferenceSwitchToRight(const size_t motor);
-
-  bool positionLatched(const size_t motor);
-
-  struct InterfaceConfiguration
-  {
-    uint8_t inv_ref : 1;
-    uint8_t sdo_int : 1;
-    uint8_t step_half : 1;
-    uint8_t inv_stp : 1;
-    uint8_t inv_dir : 1;
-    uint8_t en_sd : 1;
-    uint8_t pos_comp_sel : 2;
-    uint8_t en_refr : 1;
-    uint8_t space : 7;
-  };
-
-  InterfaceConfiguration getInterfaceConfiguration();
-
-  void setReferenceActiveLow();
-  void setReferenceActiveHigh();
-
-  void enableInverseStepPolarity();
-  void disableInverseStepPolarity();
-
-  void enableInverseDirPolarity();
-  void disableInverseDirPolarity();
-
-  void setPositionCompareMotor(const size_t motor);
-
-  void enableRightReferences();
-  void disableRightReferences();
-
-  struct SwitchState
-  {
-    uint8_t r0 : 1;
-    uint8_t l0 : 1;
-    uint8_t r1 : 1;
-    uint8_t l1 : 1;
-    uint8_t r2 : 1;
-    uint8_t l2 : 1;
-    uint8_t space : 2;
-  };
-
-  SwitchState getSwitchState();
-
-  struct ClockConfiguration
-  {
-    uint8_t usrs : 3;
-    uint8_t space0 : 5;
-    uint8_t ramp_div : 4;
-    uint8_t pulse_div : 4;
-  };
-
-  ClockConfiguration getClockConfiguration(const size_t motor);
-  double getProportionalityFactor(const size_t motor);
-
-  double getStepTimeInMicroS();
 
 private:
   // SPISettings
@@ -170,229 +45,66 @@ private:
   const static uint8_t SPI_BIT_ORDER = MSBFIRST;
   const static uint8_t SPI_MODE = SPI_MODE3;
 
-  const static uint8_t MOTOR_COUNT = 3;
-
-  const static uint32_t VERSION = 0x429101;
-
-  const static uint8_t CLOCK_FREQUENCY_MAX = 32;
-  const static uint8_t PULSE_DIV_MAX = 13;
-  const static uint8_t STEP_DIV_MAX = 15;
-  const static uint32_t MHZ_PER_HZ = 1000000;
-  const static uint32_t VELOCITY_CONSTANT = 65536;
-  const static uint32_t VELOCITY_REGISTER_MAX = 2047;
-  const static uint8_t RAMP_DIV_MAX = 13;
-  const static uint32_t ACCELERATION_REGISTER_MAX = 2047;
-  const static uint32_t ACCELERATION_CONSTANT = 536870912; // (1 << 29)
-
-  Status status_;
-  uint8_t clock_frequency_;
-  uint8_t pulse_div_[MOTOR_COUNT];
-  uint8_t ramp_div_[MOTOR_COUNT];
-
   // MOSI Datagrams
   union MosiDatagram
   {
     struct Fields
     {
-      uint32_t data : 24;
-      uint8_t rw : 1;
-      uint8_t address : 4;
-      uint8_t smda : 2;
-      uint8_t rrs : 1;
+      uint32_t data : 20;
+      uint16_t space : 12;
     } fields;
     uint32_t uint32;
   };
-  const static uint8_t RW_READ = 1;
-  const static uint8_t RW_WRITE = 0;
 
-  // IDX Addresses
-  const static uint8_t ADDRESS_X_TARGET = 0b0000;
-  const static uint8_t ADDRESS_X_ACTUAL = 0b0001;
-  const static uint8_t ADDRESS_V_MIN = 0b0010;
-  const static uint8_t ADDRESS_V_MAX = 0b0011;
-  const static uint8_t ADDRESS_V_TARGET = 0b0100;
-  const static uint8_t ADDRESS_V_ACTUAL = 0b0101;
-  const static uint8_t ADDRESS_A_MAX = 0b0110;
-  const static uint8_t ADDRESS_A_ACTUAL = 0b0111;
-  const static uint8_t ADDRESS_A_THRESHOLD = 0b1000;
-  const static uint8_t ADDRESS_PROP_FACTOR = 0b1001;
-  const static uint8_t ADDRESS_REF_CONF_MODE = 0b1010;
-  const static uint8_t ADDRESS_INTERRUPT = 0b1011;
-  const static uint8_t ADDRESS_CLOCK_CONFIGURATION = 0b1100;
-  const static uint8_t ADDRESS_DX_REF_TOLERANCE = 0b1101;
-  const static uint8_t ADDRESS_X_LATCHED = 0b1110;
-  const static uint8_t ADDRESS_USTEP_COUNT_429 = 0b1111;
-
-  // JDX Addresses
-  const static uint8_t ADDRESS_DATAGRAM_LOW_WORD = 0b0000;
-  const static uint8_t ADDRESS_DATAGRAM_HIGH_WORD = 0b0001;
-  const static uint8_t ADDRESS_COVER_POS_LEN = 0b0010;
-  const static uint8_t ADDRESS_COVER_DATAGRAM = 0b0011;
-  const static uint8_t ADDRESS_IF_CONFIGURATION_429 = 0b0100;
-  const static uint8_t ADDRESS_POS_COMP_429 = 0b0101;
-  const static uint8_t ADDRESS_POS_COMP_INT = 0b0110;
-  const static uint8_t ADDRESS_POWER_DOWN = 0b1000;
-  const static uint8_t ADDRESS_TYPE_VERSION_429 = 0b1001;
-  const static uint8_t ADDRESS_SWITCHES = 0b1110;
-  const static uint8_t ADDRESS_GLOBAL_PARAMETERS = 0b1111;
-
-  const static uint8_t SMDA_COMMON = 0b11;
-
-  const static uint8_t RRS_REGISTER = 0;
-  const static uint8_t RRS_RAM = 1;
+  // Addresses
+  const static uint8_t ADDRESS_DRVCTRL = 0b00;
+  const static uint8_t ADDRESS_CHOPCONF = 0b100;
+  const static uint8_t ADDRESS_SMARTEN = 0b101;
+  const static uint8_t ADDRESS_SGCSCONF = 0b110;
+  const static uint8_t ADDRESS_DRVCONF = 0b111;
 
   // MISO Datagrams
   union MisoDatagram
   {
     struct Fields
     {
-      uint32_t data : 24;
+      uint16_t space0 : 12;
       Status status;
+      uint8_t space1 : 2;
+      uint32_t data : 10;
     } fields;
     uint32_t uint32;
   };
-
-  // Masks
-  const static uint8_t STEP_DIV_MASK = 0xf;
-
-  // Bit Count
-  const static uint8_t X_BIT_COUNT = 24;
-  const static uint8_t V_BIT_COUNT = 12;
-  const static uint8_t A_BIT_COUNT = 12;
 
   // Union Structs
-  union PropFactor
+  union DrvConf
   {
     struct Fields
     {
-      uint8_t pdiv : 4;
       uint8_t space0 : 4;
-      uint8_t pmul : 8;
-      uint8_t space1 : 8;
-      uint8_t space2 : 8;
+      uint8_t rdsel : 2;
+      uint8_t vsense : 1;
+      uint8_t sdoff : 1;
+      uint8_t ts2g : 2;
+      uint8_t diss2g : 1;
+      uint8_t space1 : 1;
+      uint8_t slpl : 2;
+      uint8_t slph : 2;
+      uint8_t test : 1;
+      uint8_t address : 3;
+      uint16_t space3 : 12;
     } fields;
     uint32_t uint32;
   };
-  union RefConfMode
-  {
-    struct Fields
-    {
-      uint8_t mode : 2;
-      uint8_t space0 : 6;
-      ReferenceConfiguration ref_conf;
-      uint8_t lp : 1;
-      uint8_t space1 : 7;
-      uint8_t space2 : 8;
-    } fields;
-    uint32_t uint32;
-  };
-  union IfConf
-  {
-    struct Fields
-    {
-      InterfaceConfiguration if_conf;
-      uint16_t space0 : 16;
-    } fields;
-    uint32_t uint32;
-  };
-  union SwState
-  {
-    struct Fields
-    {
-      SwitchState switch_state;
-      uint16_t space0 : 16;
-    } fields;
-    uint32_t uint32;
-  };
-  union GlobalParameters
-  {
-    struct Fields
-    {
-      uint8_t lsmd : 2;
-      uint8_t nscs_s : 1;
-      uint8_t sck_s : 1;
-      uint8_t ph_ab : 1;
-      uint8_t fd_ab : 1;
-      uint8_t dac_ab : 1;
-      uint8_t cs_com_ind : 1;
-      uint8_t clk2_div : 8;
-      uint8_t cont_update : 1;
-      uint8_t space0 : 3;
-      uint8_t ref_mux : 1;
-      uint8_t mot1r : 1;
-      uint8_t space1 : 2;
-    } fields;
-    uint32_t uint32;
-  };
-  union ClkConfig
-  {
-    struct Fields
-    {
-      ClockConfiguration clk_config;
-      uint16_t space0 : 16;
-    } fields;
-    uint32_t uint32;
-  };
+
+  const static uint8_t RDSEL_MICROSTEP = 0b00;
+  const static uint8_t RDSEL_SG = 0b01;
+  const static uint8_t RDSEL_SGCS = 0b11;
 
   size_t cs_pin_;
+  Status status_;
 
-  uint32_t readRegister(const uint8_t smda,
-                        const uint8_t address);
-  void writeRegister(const uint8_t smda,
-                     const uint8_t address,
-                     const uint32_t data);
-  MisoDatagram writeRead(const MosiDatagram datagram_write);
-
-  int32_t unsignedToSigned(uint32_t input_value, uint8_t num_bits);
-
-  void specifyClockFrequencyInMHz(const uint8_t clock_frequency);
-
-  void setOptimalStepDiv(const uint32_t velocity_max_hz);
-  uint8_t getStepDiv();
-  void setStepDiv(const uint8_t step_div);
-  double stepDivToStepTime(const uint8_t step_div);
-
-  int32_t convertVelocityToHz(const size_t motor,
-                              const int16_t velocity);
-  int16_t convertVelocityFromHz(const size_t motor,
-                                const int32_t velocity);
-
-  void setOptimalPulseDiv(const size_t motor,
-                          const uint32_t velocity_max_hz);
-
-  uint16_t getVelocityMin(const size_t motor);
-  void setVelocityMin(const size_t motor,
-                      const uint16_t velocity);
-
-  uint16_t getVelocityMax(const size_t motor);
-  void setVelocityMax(const size_t motor,
-                      const uint16_t velocity);
-
-  int16_t getVelocityTarget(const size_t motor);
-  void setVelocityTarget(const size_t motor,
-                         const int16_t velocity);
-
-  int16_t getVelocityActual(const size_t motor);
-
-  int32_t convertAccelerationToHzPerS(const size_t motor,
-                                      const int16_t acceleration);
-  int16_t convertAccelerationFromHzPerS(const size_t motor,
-                                        const int32_t acceleration);
-
-  void setOptimalRampDiv(const size_t motor,
-                         const uint32_t acceleration_max_hz_per_s);
-
-  uint16_t getAccelerationMaxUpperLimit(const size_t motor);
-  uint16_t getAccelerationMaxLowerLimit(const size_t motor);
-
-  uint16_t getAccelerationMax(const size_t motor);
-  uint16_t setAccelerationMax(const size_t motor,
-                              const uint16_t acceleration);
-
-  int16_t getAccelerationActual(const size_t motor);
-
-  void setOptimalPropFactor(const size_t motor,
-                            const uint16_t acceleration_max);
+  MisoDatagram writeRead(const uint32_t data);
 
 };
 
--- examples/TestConnection/TestConnection.ino
+++ examples/TestConnection/TestConnection.ino
@@ -5,149 +5,149 @@
 
 const int BAUDRATE = 115200;
 const int LOOP_DELAY = 2000;
-const int CS_PIN = 10;
-const int CLOCK_FREQUENCY_MHZ = 32;
-const int MOTOR = 0;
+const int CS_PIN = 9;
 
 // Instantiate TMC26X
-TMC26X step_dir_controller;
+TMC26X stepper_driver;
 
 void setup()
 {
   // Setup serial communications
   Serial.begin(BAUDRATE);
 
-  step_dir_controller.setup(CS_PIN,CLOCK_FREQUENCY_MHZ);
+  stepper_driver.setup(CS_PIN);
 
-  uint32_t version = step_dir_controller.getVersion();
-  Serial << "version: " << _HEX(version) << "\n";
+  // uint32_t version = stepper_driver.getVersion();
+  // Serial << "version: " << _HEX(version) << "\n";
 
-  bool check_version = step_dir_controller.checkVersion();
-  Serial << "check_version: " << check_version << "\n";
+  // bool check_version = stepper_driver.checkVersion();
+  // Serial << "check_version: " << check_version << "\n";
 
-  step_dir_controller.setStepDirOutput();
+  // stepper_driver.setStepDirInput();
 
 }
 
 void loop()
 {
-  // step_dir_controller.setMode(MOTOR,TMC26X::VELOCITY_MODE);
-  step_dir_controller.setMode(MOTOR,TMC26X::RAMP_MODE);
+  stepper_driver.setStepDirInput();
 
-  TMC26X::Mode mode = step_dir_controller.getMode(MOTOR);
-  if (mode == TMC26X::RAMP_MODE)
-  {
-    Serial << "mode: ramp_mode\n";
-  }
-  else if (mode == TMC26X::SOFT_MODE)
-  {
-    Serial << "mode: soft_mode\n";
-  }
-  else if (mode == TMC26X::VELOCITY_MODE)
-  {
-    Serial << "mode: velocity_mode\n";
-  }
-  else if (mode == TMC26X::HOLD_MODE)
-  {
-    Serial << "mode: hold_mode\n";
-  }
+  // stepper_driver.setMode(MOTOR,TMC26X::VELOCITY_MODE);
+  // stepper_driver.setMode(MOTOR,TMC26X::RAMP_MODE);
 
-  uint32_t velocity_max_upper_limit = step_dir_controller.getVelocityMaxUpperLimitInHz();
-  Serial << "velocity_max_upper_limit: " << velocity_max_upper_limit << "\n";
+  // TMC26X::Mode mode = stepper_driver.getMode(MOTOR);
+  // if (mode == TMC26X::RAMP_MODE)
+  // {
+  //   Serial << "mode: ramp_mode\n";
+  // }
+  // else if (mode == TMC26X::SOFT_MODE)
+  // {
+  //   Serial << "mode: soft_mode\n";
+  // }
+  // else if (mode == TMC26X::VELOCITY_MODE)
+  // {
+  //   Serial << "mode: velocity_mode\n";
+  // }
+  // else if (mode == TMC26X::HOLD_MODE)
+  // {
+  //   Serial << "mode: hold_mode\n";
+  // }
 
-  uint32_t velocity_min = 100;
-  uint32_t velocity_max = 2000;
-  uint32_t acceleration_max = 20000;
-  step_dir_controller.setLimitsInHz(MOTOR,velocity_min,velocity_max,acceleration_max);
-  Serial << "setLimitsInHz: " << velocity_min << ", " << velocity_max << ", " << acceleration_max << "\n";
+  // uint32_t velocity_max_upper_limit = stepper_driver.getVelocityMaxUpperLimitInHz();
+  // Serial << "velocity_max_upper_limit: " << velocity_max_upper_limit << "\n";
 
-  acceleration_max = step_dir_controller.getAccelerationMaxInHzPerS(MOTOR);
-  Serial << "acceleration_max: " << acceleration_max << "\n";
+  // uint32_t velocity_min = 100;
+  // uint32_t velocity_max = 2000;
+  // uint32_t acceleration_max = 20000;
+  // stepper_driver.setLimitsInHz(MOTOR,velocity_min,velocity_max,acceleration_max);
+  // Serial << "setLimitsInHz: " << velocity_min << ", " << velocity_max << ", " << acceleration_max << "\n";
 
-  acceleration_max = step_dir_controller.getAccelerationMaxUpperLimitInHzPerS(MOTOR);
-  Serial << "acceleration_max_upper_limit: " << acceleration_max << "\n";
+  // acceleration_max = stepper_driver.getAccelerationMaxInHzPerS(MOTOR);
+  // Serial << "acceleration_max: " << acceleration_max << "\n";
 
-  uint32_t acceleration_actual = step_dir_controller.getAccelerationActualInHzPerS(MOTOR);
-  Serial << "acceleration_actual: " << acceleration_actual << "\n";
+  // acceleration_max = stepper_driver.getAccelerationMaxUpperLimitInHzPerS(MOTOR);
+  // Serial << "acceleration_max_upper_limit: " << acceleration_max << "\n";
 
-  velocity_min = step_dir_controller.getVelocityMinInHz(MOTOR);
-  Serial << "velocity_min: " << velocity_min << "\n";
+  // uint32_t acceleration_actual = stepper_driver.getAccelerationActualInHzPerS(MOTOR);
+  // Serial << "acceleration_actual: " << acceleration_actual << "\n";
 
-  velocity_max = step_dir_controller.getVelocityMaxInHz(MOTOR);
-  Serial << "velocity_max: " << velocity_max << "\n";
+  // velocity_min = stepper_driver.getVelocityMinInHz(MOTOR);
+  // Serial << "velocity_min: " << velocity_min << "\n";
 
-  int16_t velocity_target = 321;
-  step_dir_controller.setVelocityTargetInHz(MOTOR,velocity_target);
-  Serial << "set velocity_target: " << velocity_target << "\n";
+  // velocity_max = stepper_driver.getVelocityMaxInHz(MOTOR);
+  // Serial << "velocity_max: " << velocity_max << "\n";
 
-  velocity_target = step_dir_controller.getVelocityTargetInHz(MOTOR);
-  Serial << "velocity_target: " << velocity_target << "\n";
+  // int16_t velocity_target = 321;
+  // stepper_driver.setVelocityTargetInHz(MOTOR,velocity_target);
+  // Serial << "set velocity_target: " << velocity_target << "\n";
 
-  int16_t velocity_actual = step_dir_controller.getVelocityActualInHz(MOTOR);
-  Serial << "velocity_actual: " << velocity_actual << "\n";
+  // velocity_target = stepper_driver.getVelocityTargetInHz(MOTOR);
+  // Serial << "velocity_target: " << velocity_target << "\n";
 
-  int32_t position_target = 12345;
-  step_dir_controller.setPositionTarget(MOTOR,position_target);
-  Serial << "set position_target: " << position_target << "\n";
+  // int16_t velocity_actual = stepper_driver.getVelocityActualInHz(MOTOR);
+  // Serial << "velocity_actual: " << velocity_actual << "\n";
 
-  position_target = step_dir_controller.getPositionTarget(MOTOR);
-  Serial << "position_target: " << position_target << "\n";
+  // int32_t position_target = 12345;
+  // stepper_driver.setPositionTarget(MOTOR,position_target);
+  // Serial << "set position_target: " << position_target << "\n";
 
-  int32_t position_actual = step_dir_controller.getPositionActual(MOTOR);
-  Serial << "position_actual: " << position_actual << "\n";
+  // position_target = stepper_driver.getPositionTarget(MOTOR);
+  // Serial << "position_target: " << position_target << "\n";
 
-  TMC26X::Status status = step_dir_controller.getStatus();
-  Serial << "status.at_target_position_0 = " << status.at_target_position_0 << "\n";
-  Serial << "status.switch_left_0 = " << status.switch_left_0 << "\n";
-  Serial << "status.at_target_position_1 = " << status.at_target_position_1 << "\n";
-  Serial << "status.switch_left_1 = " << status.switch_left_1 << "\n";
-  Serial << "status.at_target_position_2 = " << status.at_target_position_2 << "\n";
-  Serial << "status.switch_left_2 = " << status.switch_left_2 << "\n";
+  // int32_t position_actual = stepper_driver.getPositionActual(MOTOR);
+  // Serial << "position_actual: " << position_actual << "\n";
 
-  step_dir_controller.disableLeftSwitchStop(MOTOR);
-  step_dir_controller.disableRightSwitchStop(MOTOR);
-  step_dir_controller.disableSoftStop(MOTOR);
-  step_dir_controller.setReferenceSwitchToRight(MOTOR);
-  TMC26X::ReferenceConfiguration ref_conf = step_dir_controller.getReferenceConfiguration(MOTOR);
-  Serial << "ref_conf.disable_stop_l: " << ref_conf.disable_stop_l << "\n";
-  Serial << "ref_conf.disable_stop_r: " << ref_conf.disable_stop_r << "\n";
-  Serial << "ref_conf.soft_stop: " << ref_conf.soft_stop << "\n";
-  Serial << "ref_conf.ref_rnl: " << ref_conf.ref_rnl << "\n";
+  // TMC26X::Status status = stepper_driver.getStatus();
+  // Serial << "status.at_target_position_0 = " << status.at_target_position_0 << "\n";
+  // Serial << "status.switch_left_0 = " << status.switch_left_0 << "\n";
+  // Serial << "status.at_target_position_1 = " << status.at_target_position_1 << "\n";
+  // Serial << "status.switch_left_1 = " << status.switch_left_1 << "\n";
+  // Serial << "status.at_target_position_2 = " << status.at_target_position_2 << "\n";
+  // Serial << "status.switch_left_2 = " << status.switch_left_2 << "\n";
 
-  bool position_latched = step_dir_controller.positionLatched(MOTOR);
-  Serial << "position_latched: " << position_latched << "\n";
+  // stepper_driver.disableLeftSwitchStop(MOTOR);
+  // stepper_driver.disableRightSwitchStop(MOTOR);
+  // stepper_driver.disableSoftStop(MOTOR);
+  // stepper_driver.setReferenceSwitchToRight(MOTOR);
+  // TMC26X::ReferenceConfiguration ref_conf = stepper_driver.getReferenceConfiguration(MOTOR);
+  // Serial << "ref_conf.disable_stop_l: " << ref_conf.disable_stop_l << "\n";
+  // Serial << "ref_conf.disable_stop_r: " << ref_conf.disable_stop_r << "\n";
+  // Serial << "ref_conf.soft_stop: " << ref_conf.soft_stop << "\n";
+  // Serial << "ref_conf.ref_rnl: " << ref_conf.ref_rnl << "\n";
 
-  step_dir_controller.setPositionCompareMotor(MOTOR);
-  step_dir_controller.enableRightReferences();
-  TMC26X::InterfaceConfiguration if_conf = step_dir_controller.getInterfaceConfiguration();
-  Serial << "if_conf.inv_ref: " << if_conf.inv_ref << "\n";
-  Serial << "if_conf.sdo_int: " << if_conf.sdo_int << "\n";
-  Serial << "if_conf.step_half: " << if_conf.step_half << "\n";
-  Serial << "if_conf.inv_stp: " << if_conf.inv_stp << "\n";
-  Serial << "if_conf.inv_dir: " << if_conf.inv_dir << "\n";
-  Serial << "if_conf.en_sd: " << if_conf.en_sd << "\n";
-  Serial << "if_conf.pos_comp_sel: " << if_conf.pos_comp_sel << "\n";
-  Serial << "if_conf.en_refr: " << if_conf.en_refr << "\n";
+  // bool position_latched = stepper_driver.positionLatched(MOTOR);
+  // Serial << "position_latched: " << position_latched << "\n";
 
-  TMC26X::SwitchState switch_state = step_dir_controller.getSwitchState();
-  Serial << "switch_state.r0: " << switch_state.r0 << "\n";
-  Serial << "switch_state.l0: " << switch_state.l0 << "\n";
-  Serial << "switch_state.r1: " << switch_state.r1 << "\n";
-  Serial << "switch_state.l1: " << switch_state.l1 << "\n";
-  Serial << "switch_state.r2: " << switch_state.r2 << "\n";
-  Serial << "switch_state.l2: " << switch_state.l2 << "\n";
+  // stepper_driver.setPositionCompareMotor(MOTOR);
+  // stepper_driver.enableRightReferences();
+  // TMC26X::InterfaceConfiguration if_conf = stepper_driver.getInterfaceConfiguration();
+  // Serial << "if_conf.inv_ref: " << if_conf.inv_ref << "\n";
+  // Serial << "if_conf.sdo_int: " << if_conf.sdo_int << "\n";
+  // Serial << "if_conf.step_half: " << if_conf.step_half << "\n";
+  // Serial << "if_conf.inv_stp: " << if_conf.inv_stp << "\n";
+  // Serial << "if_conf.inv_dir: " << if_conf.inv_dir << "\n";
+  // Serial << "if_conf.en_sd: " << if_conf.en_sd << "\n";
+  // Serial << "if_conf.pos_comp_sel: " << if_conf.pos_comp_sel << "\n";
+  // Serial << "if_conf.en_refr: " << if_conf.en_refr << "\n";
 
-  TMC26X::ClockConfiguration clk_config = step_dir_controller.getClockConfiguration(MOTOR);
-  Serial << "clk_config.usrs: " << clk_config.usrs << "\n";
-  Serial << "clk_config.ramp_div: " << clk_config.ramp_div << "\n";
-  Serial << "clk_config.pulse_div: " << clk_config.pulse_div << "\n";
+  // TMC26X::SwitchState switch_state = stepper_driver.getSwitchState();
+  // Serial << "switch_state.r0: " << switch_state.r0 << "\n";
+  // Serial << "switch_state.l0: " << switch_state.l0 << "\n";
+  // Serial << "switch_state.r1: " << switch_state.r1 << "\n";
+  // Serial << "switch_state.l1: " << switch_state.l1 << "\n";
+  // Serial << "switch_state.r2: " << switch_state.r2 << "\n";
+  // Serial << "switch_state.l2: " << switch_state.l2 << "\n";
 
-  double proportionality_factor = step_dir_controller.getProportionalityFactor(MOTOR);
-  Serial << "proportionality_factor: " << proportionality_factor << "\n";
+  // TMC26X::ClockConfiguration clk_config = stepper_driver.getClockConfiguration(MOTOR);
+  // Serial << "clk_config.usrs: " << clk_config.usrs << "\n";
+  // Serial << "clk_config.ramp_div: " << clk_config.ramp_div << "\n";
+  // Serial << "clk_config.pulse_div: " << clk_config.pulse_div << "\n";
 
-  double step_time = step_dir_controller.getStepTimeInMicroS();
-  Serial << "step_time: " << step_time << "\n";
+  // double proportionality_factor = stepper_driver.getProportionalityFactor(MOTOR);
+  // Serial << "proportionality_factor: " << proportionality_factor << "\n";
 
-  Serial << "\n";
+  // double step_time = stepper_driver.getStepTimeInMicroS();
+  // Serial << "step_time: " << step_time << "\n";
+
+  // Serial << "\n";
   delay(LOOP_DELAY);
 }
