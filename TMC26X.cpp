eeabd35f2c9771b27dd44a4a14383c4976edf653
Peter Polidoro <peterpolidoro@gmail.com>
2017-02-21 14:00:27 -0500

    Finished basic step/dir functionality.

--- TMC26X.cpp
+++ TMC26X.cpp
@@ -20,49 +20,144 @@ void TMC26X::setup(const size_t cs_pin)
 
 void TMC26X::setStepDirInput()
 {
-  DrvConf drv_conf;
-  drv_conf.uint32 = 0;
-  drv_conf.fields.rdsel = RDSEL_MICROSTEP;
-  drv_conf.fields.vsense = 1;
-  drv_conf.fields.sdoff = 0;
-  drv_conf.fields.ts2g = 0;
-  drv_conf.fields.diss2g = 0;
-  drv_conf.fields.slpl = 0b11;
-  drv_conf.fields.slph = 0b11;
-  drv_conf.fields.test = 0;
-  drv_conf.fields.address = ADDRESS_DRVCONF;
-  Serial << "drv_conf: " << _HEX(drv_conf.uint32) << "\n";
-  // writeRead(drv_conf.uint32);
-
-  DrvContStepDir drv_cont;
-  drv_cont.uint32 = 0;
-  drv_cont.fields.mres = 0;
-  drv_cont.fields.dedge = 0;
-  drv_cont.fields.intpol = 0;
-  drv_cont.fields.address = ADDRESS_DRVCTRL;
-  Serial << "drv_cont: " << _HEX(drv_cont.uint32) << "\n";
-
-  ChopConf chop_conf;
-  chop_conf.uint32 = 0;
-  chop_conf.fields.toff = 1;
-  chop_conf.fields.hstrt = 0b011;
-  chop_conf.fields.hend = 0b0010;
-  chop_conf.fields.hdec = 0b00;
-  chop_conf.fields.rndtf = 0;
-  chop_conf.fields.chm = 0;
-  chop_conf.fields.tbl = 0b10;
-  chop_conf.fields.address = ADDRESS_CHOPCONF;
-  Serial << "chop_conf: " << _HEX(chop_conf.uint32) << "\n";
+  configDriver(RDSEL_MICROSTEP,
+               VSENSE_LOW_SENSE_POWER,
+               SDOFF_STEP_DIR,
+               TS2G_3200NS,
+               DISS2G_ENABLE_SHORT_PROTECTION,
+               SLP_MAXIMUM,
+               SLP_MAXIMUM);
 }
 
-void TMC26X::setSpiInput()
+// void TMC26X::setSpiInput()
+// {
+//   configDriver(RDSEL_MICROSTEP,
+//                VSENSE_LOW_SENSE_POWER,
+//                SDOFF_STEP_DIR,
+//                TS2G_3200NS,
+//                DISS2G_ENABLE_SHORT_PROTECTION,
+//                SLP_MAXIMUM,
+//                SLP_MAXIMUM);
+// }
+
+void TMC26X::setMicrostepsPerStepTo256()
 {
+  setDriverControlStepDir(MRES_256,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
 }
 
-TMC26X::Status TMC26X::getStatus()
+void TMC26X::setMicrostepsPerStepTo128()
 {
-  return status_;
+  setDriverControlStepDir(MRES_128,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
 }
+
+void TMC26X::setMicrostepsPerStepTo64()
+{
+  setDriverControlStepDir(MRES_064,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo32()
+{
+  setDriverControlStepDir(MRES_032,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo16()
+{
+  setDriverControlStepDir(MRES_016,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo8()
+{
+  setDriverControlStepDir(MRES_008,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo4()
+{
+  setDriverControlStepDir(MRES_004,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo2()
+{
+  setDriverControlStepDir(MRES_002,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setMicrostepsPerStepTo1()
+{
+  setDriverControlStepDir(MRES_001,
+                          DEDGE_RISING,
+                          INTPOL_DISABLE_INTERPOLATION);
+}
+
+void TMC26X::setDefaultChopperConfig()
+{
+  configChopper(1,
+                0b011,
+                0b0010,
+                0b00,
+                0,
+                0,
+                0b10);
+}
+
+void TMC26X::disableCoolStep()
+{
+  setCoolStepRegister(SEMIN_DISABLED,
+                      SEUP_1,
+                      0b00,
+                      SEDN_32,
+                      SEIMIN_HALF);
+}
+
+// void TMC26X::enableCoolStep()
+// {
+//   setCoolStepRegister(SEMIN_DISABLED,
+//                              SEUP_1,
+//                              0b00,
+//                              SEDN_32,
+//                              SEIMIN_HALF);
+// }
+
+double TMC26X::setCurrentScalePercent(uint8_t cs)
+{
+  uint8_t cs_thresholded = cs;
+  if (cs_thresholded > CURRENT_SCALE_PERCENT_MAX)
+  {
+    cs_thresholded = CURRENT_SCALE_PERCENT_MAX;
+  }
+  if (cs_thresholded < CURRENT_SCALE_PERCENT_MIN)
+  {
+    cs_thresholded = CURRENT_SCALE_PERCENT_MIN;
+  }
+  uint8_t cs_mapped = betterMap(cs_thresholded,
+                                CURRENT_SCALE_PERCENT_MIN,
+                                CURRENT_SCALE_PERCENT_MAX,
+                                CS_REGISTER_MIN,
+                                CS_REGISTER_MAX);
+  setStallGuardRegister(cs_mapped,
+                        SGT_DEFAULT,
+                        SFILT_FILTERED_MODE);
+  return (cs_mapped + 1)*100.0/32;
+}
+
+// TMC26X::Status TMC26X::getStatus()
+// {
+//   return status_;
+// }
 
 // private
 TMC26X::MisoDatagram TMC26X::writeRead(const uint32_t data)
@@ -70,7 +165,6 @@ TMC26X::MisoDatagram TMC26X::writeRead(const uint32_t data)
   MosiDatagram datagram_write;
   datagram_write.uint32 = 0;
   datagram_write.fields.data = data;
-  Serial << "mosi datagram: " << _HEX(datagram_write.uint32) << "\n";
   MisoDatagram datagram_read;
   datagram_read.uint32 = 0;
   SPI.beginTransaction(SPISettings(SPI_CLOCK,SPI_BIT_ORDER,SPI_MODE));
@@ -86,6 +180,106 @@ TMC26X::MisoDatagram TMC26X::writeRead(const uint32_t data)
   noInterrupts();
   status_ = datagram_read.fields.status;
   interrupts();
-  Serial << "miso datagram: " << _HEX(datagram_read.uint32) << "\n";
   return datagram_read;
+}
+
+void TMC26X::configDriver(const uint8_t rdsel,
+                          const uint8_t vsense,
+                          const uint8_t sdoff,
+                          const uint8_t ts2g,
+                          const uint8_t diss2g,
+                          const uint8_t slpl,
+                          const uint8_t slph)
+{
+  DrvConf drv_conf;
+  drv_conf.uint32 = 0;
+  drv_conf.fields.rdsel = rdsel;
+  drv_conf.fields.vsense = vsense;
+  drv_conf.fields.sdoff = sdoff;
+  drv_conf.fields.ts2g = ts2g;
+  drv_conf.fields.diss2g = diss2g;
+  drv_conf.fields.slpl = slpl;
+  drv_conf.fields.slph = slph;
+  drv_conf.fields.test = 0;
+  drv_conf.fields.address = ADDRESS_DRVCONF;
+  writeRead(drv_conf.uint32);
+}
+
+void TMC26X::setDriverControlStepDir(const uint8_t mres,
+                                     const uint8_t dedge,
+                                     const uint8_t intpol)
+{
+  DrvContStepDir drv_cont;
+  drv_cont.uint32 = 0;
+  drv_cont.fields.mres = mres;
+  drv_cont.fields.dedge = dedge;
+  drv_cont.fields.intpol = intpol;
+  drv_cont.fields.address = ADDRESS_DRVCTRL;
+  writeRead(drv_cont.uint32);
+}
+
+void TMC26X::configChopper(const uint8_t toff,
+                           const uint8_t hstrt,
+                           const uint8_t hend,
+                           const uint8_t hdec,
+                           const uint8_t rndtf,
+                           const uint8_t chm,
+                           const uint8_t tbl)
+{
+  ChopConf chop_conf;
+  chop_conf.uint32 = 0;
+  chop_conf.fields.toff = toff;
+  chop_conf.fields.hstrt = hstrt;
+  chop_conf.fields.hend = hend;
+  chop_conf.fields.hdec = hdec;
+  chop_conf.fields.rndtf = rndtf;
+  chop_conf.fields.chm = chm;
+  chop_conf.fields.tbl = tbl;
+  chop_conf.fields.address = ADDRESS_CHOPCONF;
+  writeRead(chop_conf.uint32);
+}
+
+void TMC26X::setCoolStepRegister(const uint8_t semin,
+                                 const uint8_t seup,
+                                 const uint8_t semax,
+                                 const uint8_t sedn,
+                                 const uint8_t seimin)
+{
+  SmartEn smart_en;
+  smart_en.uint32 = 0;
+  smart_en.fields.semin = semin;
+  smart_en.fields.seup = seup;
+  smart_en.fields.semax = semax;
+  smart_en.fields.sedn = sedn;
+  smart_en.fields.seimin = seimin;
+  smart_en.fields.address = ADDRESS_SMARTEN;
+  writeRead(smart_en.uint32);
+}
+
+void TMC26X::setStallGuardRegister(const uint8_t cs,
+                                   const int8_t sgt,
+                                   const uint8_t sfilt)
+{
+  SgcsConf sgcs_conf;
+  sgcs_conf.uint32 = 0;
+  sgcs_conf.fields.cs = cs;
+  int8_t sgt_thresholded = sgt;
+  if (sgt_thresholded > SGT_REGISTER_MAX)
+  {
+    sgt_thresholded = SGT_REGISTER_MAX;
+  }
+  if (sgt_thresholded < SGT_REGISTER_MIN)
+  {
+    sgt_thresholded = SGT_REGISTER_MIN;
+  }
+  sgcs_conf.fields.sgt = sgt_thresholded;
+  sgcs_conf.fields.sfilt = sfilt;
+  sgcs_conf.fields.address = ADDRESS_SGCSCONF;
+  writeRead(sgcs_conf.uint32);
+}
+
+long TMC26X::betterMap(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  long mapped_x = map(x,in_min,(in_max+1),out_min,(out_max+1));
+  return constrain(mapped_x,out_min,out_max);
 }
--- examples/TestConnection/TestConnection.ino
+++ examples/TestConnection/TestConnection.ino
@@ -17,147 +17,32 @@ void setup()
 
   stepper_driver.setup(CS_PIN);
 
-  // uint32_t version = stepper_driver.getVersion();
-  // Serial << "version: " << _HEX(version) << "\n";
-
-  // bool check_version = stepper_driver.checkVersion();
-  // Serial << "check_version: " << check_version << "\n";
-
-  // stepper_driver.setStepDirInput();
-
 }
 
 void loop()
 {
   stepper_driver.setStepDirInput();
-
-  TMC26X::Status status = stepper_driver.getStatus();
-  Serial << "status.stall = " << status.stall << "\n";
-  Serial << "status.over_temperature_shutdown = " << status.over_temperature_shutdown << "\n";
-  Serial << "status.over_temperature_warning = " << status.over_temperature_warning << "\n";
-  Serial << "status.short_to_ground_a = " << status.short_to_ground_a << "\n";
-  Serial << "status.short_to_ground_b = " << status.short_to_ground_b << "\n";
-  Serial << "status.open_load_a = " << status.open_load_a << "\n";
-  Serial << "status.open_load_b = " << status.open_load_b << "\n";
-  Serial << "status.standstill = " << status.standstill << "\n";
-
-  // stepper_driver.setMode(MOTOR,TMC26X::VELOCITY_MODE);
-  // stepper_driver.setMode(MOTOR,TMC26X::RAMP_MODE);
-
-  // TMC26X::Mode mode = stepper_driver.getMode(MOTOR);
-  // if (mode == TMC26X::RAMP_MODE)
-  // {
-  //   Serial << "mode: ramp_mode\n";
-  // }
-  // else if (mode == TMC26X::SOFT_MODE)
-  // {
-  //   Serial << "mode: soft_mode\n";
-  // }
-  // else if (mode == TMC26X::VELOCITY_MODE)
-  // {
-  //   Serial << "mode: velocity_mode\n";
-  // }
-  // else if (mode == TMC26X::HOLD_MODE)
-  // {
-  //   Serial << "mode: hold_mode\n";
-  // }
-
-  // uint32_t velocity_max_upper_limit = stepper_driver.getVelocityMaxUpperLimitInHz();
-  // Serial << "velocity_max_upper_limit: " << velocity_max_upper_limit << "\n";
-
-  // uint32_t velocity_min = 100;
-  // uint32_t velocity_max = 2000;
-  // uint32_t acceleration_max = 20000;
-  // stepper_driver.setLimitsInHz(MOTOR,velocity_min,velocity_max,acceleration_max);
-  // Serial << "setLimitsInHz: " << velocity_min << ", " << velocity_max << ", " << acceleration_max << "\n";
-
-  // acceleration_max = stepper_driver.getAccelerationMaxInHzPerS(MOTOR);
-  // Serial << "acceleration_max: " << acceleration_max << "\n";
-
-  // acceleration_max = stepper_driver.getAccelerationMaxUpperLimitInHzPerS(MOTOR);
-  // Serial << "acceleration_max_upper_limit: " << acceleration_max << "\n";
-
-  // uint32_t acceleration_actual = stepper_driver.getAccelerationActualInHzPerS(MOTOR);
-  // Serial << "acceleration_actual: " << acceleration_actual << "\n";
-
-  // velocity_min = stepper_driver.getVelocityMinInHz(MOTOR);
-  // Serial << "velocity_min: " << velocity_min << "\n";
-
-  // velocity_max = stepper_driver.getVelocityMaxInHz(MOTOR);
-  // Serial << "velocity_max: " << velocity_max << "\n";
-
-  // int16_t velocity_target = 321;
-  // stepper_driver.setVelocityTargetInHz(MOTOR,velocity_target);
-  // Serial << "set velocity_target: " << velocity_target << "\n";
-
-  // velocity_target = stepper_driver.getVelocityTargetInHz(MOTOR);
-  // Serial << "velocity_target: " << velocity_target << "\n";
-
-  // int16_t velocity_actual = stepper_driver.getVelocityActualInHz(MOTOR);
-  // Serial << "velocity_actual: " << velocity_actual << "\n";
-
-  // int32_t position_target = 12345;
-  // stepper_driver.setPositionTarget(MOTOR,position_target);
-  // Serial << "set position_target: " << position_target << "\n";
-
-  // position_target = stepper_driver.getPositionTarget(MOTOR);
-  // Serial << "position_target: " << position_target << "\n";
-
-  // int32_t position_actual = stepper_driver.getPositionActual(MOTOR);
-  // Serial << "position_actual: " << position_actual << "\n";
+  Serial << "setStepDirInput\n";
+  stepper_driver.setMicrostepsPerStepTo256();
+  Serial << "setMicrostepsPerStepTo256\n";
+  stepper_driver.setDefaultChopperConfig();
+  Serial << "setDefaultChopperConfig\n";
+  stepper_driver.disableCoolStep();
+  Serial << "disableCoolStep\n";
+  uint8_t current_scale_set = 18;
+  double current_scale_actual = stepper_driver.setCurrentScalePercent(current_scale_set);
+  Serial << "current_scale_percent set: " << current_scale_set << ", actual: " << current_scale_actual << "\n";
 
   // TMC26X::Status status = stepper_driver.getStatus();
-  // Serial << "status.at_target_position_0 = " << status.at_target_position_0 << "\n";
-  // Serial << "status.switch_left_0 = " << status.switch_left_0 << "\n";
-  // Serial << "status.at_target_position_1 = " << status.at_target_position_1 << "\n";
-  // Serial << "status.switch_left_1 = " << status.switch_left_1 << "\n";
-  // Serial << "status.at_target_position_2 = " << status.at_target_position_2 << "\n";
-  // Serial << "status.switch_left_2 = " << status.switch_left_2 << "\n";
+  // Serial << "status.stall = " << status.stall << "\n";
+  // Serial << "status.over_temperature_shutdown = " << status.over_temperature_shutdown << "\n";
+  // Serial << "status.over_temperature_warning = " << status.over_temperature_warning << "\n";
+  // Serial << "status.short_to_ground_a = " << status.short_to_ground_a << "\n";
+  // Serial << "status.short_to_ground_b = " << status.short_to_ground_b << "\n";
+  // Serial << "status.open_load_a = " << status.open_load_a << "\n";
+  // Serial << "status.open_load_b = " << status.open_load_b << "\n";
+  // Serial << "status.standstill = " << status.standstill << "\n";
 
-  // stepper_driver.disableLeftSwitchStop(MOTOR);
-  // stepper_driver.disableRightSwitchStop(MOTOR);
-  // stepper_driver.disableSoftStop(MOTOR);
-  // stepper_driver.setReferenceSwitchToRight(MOTOR);
-  // TMC26X::ReferenceConfiguration ref_conf = stepper_driver.getReferenceConfiguration(MOTOR);
-  // Serial << "ref_conf.disable_stop_l: " << ref_conf.disable_stop_l << "\n";
-  // Serial << "ref_conf.disable_stop_r: " << ref_conf.disable_stop_r << "\n";
-  // Serial << "ref_conf.soft_stop: " << ref_conf.soft_stop << "\n";
-  // Serial << "ref_conf.ref_rnl: " << ref_conf.ref_rnl << "\n";
-
-  // bool position_latched = stepper_driver.positionLatched(MOTOR);
-  // Serial << "position_latched: " << position_latched << "\n";
-
-  // stepper_driver.setPositionCompareMotor(MOTOR);
-  // stepper_driver.enableRightReferences();
-  // TMC26X::InterfaceConfiguration if_conf = stepper_driver.getInterfaceConfiguration();
-  // Serial << "if_conf.inv_ref: " << if_conf.inv_ref << "\n";
-  // Serial << "if_conf.sdo_int: " << if_conf.sdo_int << "\n";
-  // Serial << "if_conf.step_half: " << if_conf.step_half << "\n";
-  // Serial << "if_conf.inv_stp: " << if_conf.inv_stp << "\n";
-  // Serial << "if_conf.inv_dir: " << if_conf.inv_dir << "\n";
-  // Serial << "if_conf.en_sd: " << if_conf.en_sd << "\n";
-  // Serial << "if_conf.pos_comp_sel: " << if_conf.pos_comp_sel << "\n";
-  // Serial << "if_conf.en_refr: " << if_conf.en_refr << "\n";
-
-  // TMC26X::SwitchState switch_state = stepper_driver.getSwitchState();
-  // Serial << "switch_state.r0: " << switch_state.r0 << "\n";
-  // Serial << "switch_state.l0: " << switch_state.l0 << "\n";
-  // Serial << "switch_state.r1: " << switch_state.r1 << "\n";
-  // Serial << "switch_state.l1: " << switch_state.l1 << "\n";
-  // Serial << "switch_state.r2: " << switch_state.r2 << "\n";
-  // Serial << "switch_state.l2: " << switch_state.l2 << "\n";
-
-  // TMC26X::ClockConfiguration clk_config = stepper_driver.getClockConfiguration(MOTOR);
-  // Serial << "clk_config.usrs: " << clk_config.usrs << "\n";
-  // Serial << "clk_config.ramp_div: " << clk_config.ramp_div << "\n";
-  // Serial << "clk_config.pulse_div: " << clk_config.pulse_div << "\n";
-
-  // double proportionality_factor = stepper_driver.getProportionalityFactor(MOTOR);
-  // Serial << "proportionality_factor: " << proportionality_factor << "\n";
-
-  // double step_time = stepper_driver.getStepTimeInMicroS();
-  // Serial << "step_time: " << step_time << "\n";
-
-  // Serial << "\n";
+  Serial << "\n";
   delay(LOOP_DELAY);
 }
